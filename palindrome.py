e93baec83c75afa48d858be1c77af7103d295e25
NoyP <96237612+NoyPhilosof@users.noreply.github.com>
2022-01-18 13:44:54 +0200

    fixed indentation in is_palindrome func

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,7 @@
-def is_palindrome(user_string):
-  backwards_string = user_string[::-1]
-  is_it_palindrome = (backwards_string == user_string)
-  return is_it_palindrome
+def is_palindrome(some_string):
+    backwards_string = some_string[::-1]
+    is_it_palindrome = (backwards_string == some_string)
+    return is_it_palindrome
 
 
 user_string = input("Please enter a string: ")
